50a6e60ae461931ae5e35bbf28216f8caeca0766
Nick Cromwell <cromwellnick@gmail.com>
2019-09-01 00:23:22 -0400

    Formatting, Update Rabbitmq package

--- src/Ranger.ApiGateway/Startup.cs
+++ src/Ranger.ApiGateway/Startup.cs
@@ -18,51 +18,61 @@ using Ranger.ApiGateway.Data;
 using Ranger.InternalHttpClient;
 using Ranger.RabbitMQ;
 
-namespace Ranger.ApiGateway {
-    public class Startup {
+namespace Ranger.ApiGateway
+{
+    public class Startup
+    {
         private readonly IConfiguration configuration;
         private readonly ILoggerFactory loggerFactory;
         private readonly ILogger<Startup> logger;
         private IContainer container;
 
-        public Startup (IConfiguration configuration, ILoggerFactory loggerFactory, ILogger<Startup> logger) {
+        public Startup(IConfiguration configuration, ILoggerFactory loggerFactory, ILogger<Startup> logger)
+        {
             this.configuration = configuration;
             this.loggerFactory = loggerFactory;
             this.logger = logger;
         }
 
-        public IServiceProvider ConfigureServices (IServiceCollection services) {
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor> ();
-            services.AddSingleton<ITenantsClient, TenantsClient> (provider => {
-                return new TenantsClient ("http://tenants:8082", loggerFactory.CreateLogger<TenantsClient> ());
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<ITenantsClient, TenantsClient>(provider =>
+            {
+                return new TenantsClient("http://tenants:8082", loggerFactory.CreateLogger<TenantsClient>());
             });
-            services.AddSingleton<IIdentityClient, IdentityClient> (provider => {
-                return new IdentityClient ("http://identity:5000", loggerFactory.CreateLogger<IdentityClient> ());
+            services.AddSingleton<IIdentityClient, IdentityClient>(provider =>
+            {
+                return new IdentityClient("http://identity:5000", loggerFactory.CreateLogger<IdentityClient>());
             });
-            services.AddCors ();
-            services.AddMvc (options => {
-                    var policy = ScopePolicy.Create ("apiGateway");
-                    options.Filters.Add (new AuthorizeFilter (policy));
-                })
-                .SetCompatibilityVersion (CompatibilityVersion.Version_2_2)
-                .AddJsonOptions (options => {
-                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver ();
+            services.AddCors();
+            services.AddMvc(options =>
+            {
+                var policy = ScopePolicy.Create("apiGateway");
+                options.Filters.Add(new AuthorizeFilter(policy));
+            })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
-            services.AddEntityFrameworkNpgsql ().AddDbContext<ApiGatewayDbContext> (options => {
-                    options.UseNpgsql (configuration["cloudSql:ConnectionString"]);
-                },
+            services.AddEntityFrameworkNpgsql().AddDbContext<ApiGatewayDbContext>(options =>
+            {
+                options.UseNpgsql(configuration["cloudSql:ConnectionString"]);
+            },
                 ServiceLifetime.Transient
             );
-            services.AddTransient<IApiGatewayDbContextInitializer, ApiGatewayDbContextInitializer> ();
+            services.AddTransient<IApiGatewayDbContextInitializer, ApiGatewayDbContextInitializer>();
 
-            services.AddDataProtection ()
-                .ProtectKeysWithCertificate (new X509Certificate2 (configuration["DataProtectionCertPath:Path"]))
-                .PersistKeysToDbContext<ApiGatewayDbContext> ();
+            services.AddDataProtection()
+                .ProtectKeysWithCertificate(new X509Certificate2(configuration["DataProtectionCertPath:Path"]))
+                .PersistKeysToDbContext<ApiGatewayDbContext>();
 
-            services.AddAuthentication ("Bearer")
-                .AddIdentityServerAuthentication (options => {
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
                     options.Authority = "http://identity:5000/auth";
                     options.ApiName = "apiGateway";
 
@@ -71,26 +81,29 @@ namespace Ranger.ApiGateway {
                     options.RequireHttpsMetadata = false;
                 });
 
-            var builder = new ContainerBuilder ();
-            builder.Populate (services);
-            builder.AddRabbitMq (loggerFactory);
-            container = builder.Build ();
-            return new AutofacServiceProvider (container);
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            builder.AddRabbitMq(loggerFactory);
+            container = builder.Build();
+            return new AutofacServiceProvider(container);
         }
 
-        public void Configure (IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
-            app.UseCors (builder => {
-                builder.AllowAnyHeader ()
-                    .AllowAnyMethod ()
-                    .AllowAnyOrigin ()
-                    .WithExposedHeaders ("X-Operation");
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin()
+                    .WithExposedHeaders("X-Operation");
             });
 
-            if (env.IsProduction ()) {
-                app.UsePathBase ("/api");
+            if (env.IsProduction())
+            {
+                app.UsePathBase("/api");
             }
-            app.UseAuthentication ();
-            app.UseMvcWithDefaultRoute ();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
