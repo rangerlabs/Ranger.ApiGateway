ab86d6a014e086ce342cae4154674b02e752a883
Nick Cromwell <nick.cromwell@rangerlabs.io>
2019-09-07 22:51:01 -0400

    Dev (#3)
    * Don't publish image on PR; add dev branch build
    
    * formatting
    
    * add "touch" file to /app/vsdbg for prod builds
    
    * fix publish script
    
    * update travis secure value
    
    * Add Ranger.ApiGateway.Data and cert to support data protection. Data protection with a backing repo is necessary to support decrypting cookies accross the kubernetes cluster
    
    * Remove residual email services; upgrade common package
    
    * Formatting, Update Rabbitmq package
    
    * update rabbitmq
    
    * update rabbitmq
    
    * Update common package reference

--- src/Ranger.ApiGateway/Controllers/BaseController/BaseController.cs
+++ src/Ranger.ApiGateway/Controllers/BaseController/BaseController.cs
@@ -3,12 +3,15 @@ using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 using Ranger.RabbitMQ;
 
-namespace Ranger.ApiGateway {
-    [Route ("[controller]")]
+namespace Ranger.ApiGateway
+{
+    [Route("[controller]")]
     [ApiController]
-    public class BaseController : ControllerBase {
+    public class BaseController : ControllerBase
+    {
         private static readonly string AcceptLanguageHeader = "accept-language";
         private static readonly string OperationHeader = "X-Operation";
         private static readonly string ResourceHeader = "X-Resource";
@@ -18,45 +21,59 @@ namespace Ranger.ApiGateway {
         private readonly IBusPublisher busPublisher;
         protected readonly ILogger Logger;
 
-        public BaseController (IBusPublisher busPublisher, ILogger logger) {
+        public BaseController(IBusPublisher busPublisher, ILogger logger)
+        {
             this.busPublisher = busPublisher;
             this.Logger = logger;
         }
 
-        protected IActionResult Send<T> (T command, Guid? resourceId = null, string resource = "") where T : ICommand {
-            var context = GetContext<T> (resourceId, resource);
-            try {
-                busPublisher.Send (command, context);
-                return Accepted (context);
-            } catch (Exception ex) {
-                Logger.LogError (ex, "An exception occurred publishing a command. Ensure the service is connect to a running RabbitMQ instance.");
-                return InternalServerError ();
+        protected IActionResult Send<T>(T command, Guid? resourceId = null, string resource = "") where T : ICommand
+        {
+            var context = GetContext<T>(resourceId, resource);
+            try
+            {
+                busPublisher.Send(command, context);
+                return Accepted(context);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "An exception occurred publishing a command. Ensure the service is connect to a running RabbitMQ instance.");
+                return InternalServerError();
             }
         }
 
-        protected IActionResult Accepted (ICorrelationContext context) {
-            Response.Headers.Add (OperationHeader, $"operations/{context.CorrelationContextId}");
-            if (!string.IsNullOrWhiteSpace (context.Resource)) {
-                Response.Headers.Add (ResourceHeader, context.Resource);
+        protected IActionResult Accepted(ICorrelationContext context)
+        {
+            Response.Headers.Add(OperationHeader, $"operations/{context.CorrelationContextId}");
+            if (!string.IsNullOrWhiteSpace(context.Resource))
+            {
+                Response.Headers.Add(ResourceHeader, context.Resource);
             }
 
-            return base.Accepted ();
+            return base.Accepted();
         }
 
-        protected IActionResult InternalServerError (string errors = "") {
-            return StatusCode (StatusCodes.Status500InternalServerError, new { errors = errors });
+        protected IActionResult InternalServerError(string errors = "")
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new { errors = errors });
         }
 
-        private ICorrelationContext GetContext<T> (Guid? resourceId, string resource) {
-            if (!string.IsNullOrWhiteSpace (resource)) {
+        private ICorrelationContext GetContext<T>(Guid? resourceId, string resource)
+        {
+            if (!string.IsNullOrWhiteSpace(resource))
+            {
                 resource = $"{resource}/{resourceId}";
             }
 
-            return CorrelationContext.Create<T> (
-                Guid.NewGuid (),
-                UserId,
+            StringValues domain;
+            bool success = HttpContext.Request.Headers.TryGetValue("X-Tenant-Domain", out domain);
+
+            return CorrelationContext.Create<T>(
+                Guid.NewGuid(),
+                success ? domain.First() : "",
+                UserEmail,
                 resourceId ?? Guid.Empty,
-                Request.Path.ToString (),
+                Request.Path.ToString(),
                 HttpContext.TraceIdentifier,
                 "",
                 this.HttpContext.Connection.Id,
@@ -64,21 +81,27 @@ namespace Ranger.ApiGateway {
             );
         }
 
-        protected string UserId {
-            get {
-                return string.IsNullOrWhiteSpace (User?.Identity?.Name) ? "" : User.Identity.Name;
+        protected string UserEmail
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(User?.Identity?.Name) ? "" : User.Identity.Name;
             }
         }
 
-        protected string Culture {
-            get {
-                return Request.Headers.ContainsKey (AcceptLanguageHeader) ? Request.Headers[AcceptLanguageHeader].First ().ToLowerInvariant () : DefaultCulture;
+        protected string Culture
+        {
+            get
+            {
+                return Request.Headers.ContainsKey(AcceptLanguageHeader) ? Request.Headers[AcceptLanguageHeader].First().ToLowerInvariant() : DefaultCulture;
             }
         }
 
-        protected string Domain {
-            get {
-                return Request.Headers["X-Tenant-Domain"].First ();
+        protected string Domain
+        {
+            get
+            {
+                return Request.Headers["X-Tenant-Domain"].First();
             }
         }
     }
