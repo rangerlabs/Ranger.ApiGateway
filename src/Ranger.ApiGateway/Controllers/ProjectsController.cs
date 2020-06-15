ccf874340a7fc2e8acdf6b601302e812f9f37844
Nick Cromwell <cromwellnick@gmail.com>
2020-06-15 00:01:12 -0400

    update response message

--- src/Ranger.ApiGateway/Controllers/ProjectsController.cs
+++ src/Ranger.ApiGateway/Controllers/ProjectsController.cs
@@ -12,16 +12,19 @@ using Ranger.Common;
 using Ranger.InternalHttpClient;
 using Ranger.RabbitMQ;
 
-namespace Ranger.ApiGateway {
-    [ApiVersion ("1.0")]
+namespace Ranger.ApiGateway
+{
+    [ApiVersion("1.0")]
     [ApiController]
-    [Authorize (Policy = "TenantIdResolved")]
-    public class ProjectsController : BaseController<ProjectsController> {
+    [Authorize(Policy = "TenantIdResolved")]
+    public class ProjectsController : BaseController<ProjectsController>
+    {
         private readonly IBusPublisher busPublisher;
         private readonly ProjectsHttpClient projectsClient;
         private readonly ILogger<ProjectsController> logger;
 
-        public ProjectsController (IBusPublisher busPublisher, ProjectsHttpClient projectsClient, ILogger<ProjectsController> logger) : base (busPublisher, logger) {
+        public ProjectsController(IBusPublisher busPublisher, ProjectsHttpClient projectsClient, ILogger<ProjectsController> logger) : base(busPublisher, logger)
+        {
             this.logger = logger;
             this.busPublisher = busPublisher;
             this.projectsClient = projectsClient;
@@ -30,13 +33,14 @@ namespace Ranger.ApiGateway {
         ///<summary>
         /// Gets all projects
         ///</summary>
-        [ProducesResponseType (StatusCodes.Status200OK)]
-        [ProducesResponseType (StatusCodes.Status404NotFound)]
-        [HttpGet ("/projects")]
-        [Authorize (Roles = "User")]
-        public async Task<ApiResponse> GetAllProjects () {
-            var apiResponse = await projectsClient.GetAllProjectsForUserAsync<IEnumerable<ProjectResponseModel>> (TenantId, UserFromClaims.Email);
-            return new ApiResponse ("Successfully retrieved all projects", apiResponse.Result);
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("/projects")]
+        [Authorize(Roles = "User")]
+        public async Task<ApiResponse> GetAllProjects()
+        {
+            var apiResponse = await projectsClient.GetAllProjectsForUserAsync<IEnumerable<ProjectResponseModel>>(TenantId, UserFromClaims.Email);
+            return new ApiResponse("Successfully retrieved all projects", apiResponse.Result);
         }
 
         ///<summary>
@@ -44,43 +48,46 @@ namespace Ranger.ApiGateway {
         ///</summary>
         ///<param name="projectId">The project's unique identifier</param>
         ///<param name="projectModel">The project model necessary to update an existing project</param>
-        [ProducesResponseType (StatusCodes.Status200OK)]
-        [ProducesResponseType (StatusCodes.Status304NotModified)]
-        [ProducesResponseType (StatusCodes.Status400BadRequest)]
-        [ProducesResponseType (StatusCodes.Status409Conflict)]
-        [HttpPut ("/projects/{projectId}")]
-        [Authorize (Roles = "Admin")]
-        public async Task<ApiResponse> UpdateProject ([FromRoute] Guid projectId, PutProjectModel projectModel) {
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status304NotModified)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [HttpPut("/projects/{projectId}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ApiResponse> UpdateProject([FromRoute] Guid projectId, PutProjectModel projectModel)
+        {
             var request = new { Name = projectModel.Name, Description = projectModel.Description, Enabled = projectModel.Enabled, Version = projectModel.Version, UserEmail = UserFromClaims.Email };
-            var apiResponse = await projectsClient.PutProjectAsync<ProjectResponseModel> (TenantId, projectId, JsonConvert.SerializeObject (request));
-            return new ApiResponse ("Successfully updated project", apiResponse.Result);
+            var apiResponse = await projectsClient.PutProjectAsync<ProjectResponseModel>(TenantId, projectId, JsonConvert.SerializeObject(request));
+            return new ApiResponse("Successfully updated project", apiResponse.Result);
         }
 
         ///<summary>
         /// Creates a new project
         ///</summary>
         ///<param name="projectModel">The project model necessary to create a new project</param>
-        [ProducesResponseType (StatusCodes.Status201Created)]
-        [HttpPost ("/projects")]
-        [Authorize (Roles = "Admin")]
-        public async Task<ApiResponse> CreateProject (PostProjectModel projectModel) {
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [HttpPost("/projects")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ApiResponse> CreateProject(PostProjectModel projectModel)
+        {
             var request = new { Name = projectModel.Name, Description = projectModel.Description, Enabled = projectModel.Enabled, UserEmail = UserFromClaims.Email };
-            var apiResponse = await projectsClient.PostProjectAsync<ProjectResponseModel> (TenantId, JsonConvert.SerializeObject (request));
-            return new ApiResponse ($"Successfully created project '{projectModel.Name}'", apiResponse.Result);
+            var apiResponse = await projectsClient.PostProjectAsync<ProjectResponseModel>(TenantId, JsonConvert.SerializeObject(request));
+            return new ApiResponse($"Successfully created project '{projectModel.Name}'", apiResponse.Result);
         }
 
         ///<summary>
         /// Deletes a project
         ///</summary>
         ///<param name="projectId">The project's unique identifier</param>
-        [ProducesResponseType (StatusCodes.Status200OK)]
-        [ProducesResponseType (StatusCodes.Status400BadRequest)]
-        [ProducesResponseType (StatusCodes.Status409Conflict)]
-        [HttpDelete ("/projects/{projectId}")]
-        [Authorize (Roles = "Admin")]
-        public async Task<ApiResponse> SoftDeleteProject (Guid projectId) {
-            var apiResponse = await projectsClient.SoftDeleteProjectAsync (TenantId, projectId, UserFromClaims.Email);
-            return new ApiResponse ("Successfully deleted project", apiResponse.Result);
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [HttpDelete("/projects/{projectId}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ApiResponse> SoftDeleteProject(Guid projectId)
+        {
+            var apiResponse = await projectsClient.SoftDeleteProjectAsync(TenantId, projectId, UserFromClaims.Email);
+            return new ApiResponse("Successfully deleted project", apiResponse.Result);
         }
 
         ///<summary>
@@ -89,15 +96,16 @@ namespace Ranger.ApiGateway {
         ///<param name="projectId">The project's unique identifier</param>
         ///<param name="environment">The environment whose API key to reset, 'live' or 'test'</param>
         ///<param name="apiKeyResetModel">The model necessary to reset an API key</param>
-        [ProducesResponseType (StatusCodes.Status200OK)]
-        [ProducesResponseType (StatusCodes.Status400BadRequest)]
-        [ProducesResponseType (StatusCodes.Status409Conflict)]
-        [HttpPut ("/projects/{projectId}/{environment}/reset")]
-        [Authorize (Roles = "Admin")]
-        public async Task<ApiResponse> ApiKeyReset (Guid projectId, EnvironmentEnum environment, ApiKeyResetModel apiKeyResetModel) {
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [HttpPut("/projects/{projectId}/{environment}/reset")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ApiResponse> ApiKeyReset(Guid projectId, EnvironmentEnum environment, ApiKeyResetModel apiKeyResetModel)
+        {
             var request = new { Version = apiKeyResetModel.Version, UserEmail = UserFromClaims.Email };
-            var apiResponse = await projectsClient.ApiKeyResetAsync<ProjectResponseModel> (TenantId, projectId, environment, JsonConvert.SerializeObject (request));
-            return new ApiResponse ("Successfully reset environment API key", apiResponse.Result);
+            var apiResponse = await projectsClient.ApiKeyResetAsync<ProjectResponseModel>(TenantId, projectId, environment, JsonConvert.SerializeObject(request));
+            return new ApiResponse("Successfully reset API key", apiResponse.Result);
         }
     }
 }
