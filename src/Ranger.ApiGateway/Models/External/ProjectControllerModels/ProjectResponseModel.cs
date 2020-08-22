b64cf4c8f4cfdd8ef782000e033fbadb3d10f56f
Nick Cromwell <cromwellnick@gmail.com>
2020-08-22 13:03:38 -0400

    ProjectId -> Id, IntegrationId -> Id

--- src/Ranger.ApiGateway/Models/External/ProjectControllerModels/ProjectResponseModel.cs
+++ src/Ranger.ApiGateway/Models/External/ProjectControllerModels/ProjectResponseModel.cs
@@ -5,7 +5,7 @@ namespace Ranger.ApiGateway
 {
     public class ProjectResponseModel
     {
-        public Guid ProjectId { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string LiveApiKey { get; set; }
