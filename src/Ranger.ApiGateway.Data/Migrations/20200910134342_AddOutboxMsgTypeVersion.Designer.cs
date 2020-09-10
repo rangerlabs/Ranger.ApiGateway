4b941884d67a347ff1631778bf0a78283a48d804
Nick Cromwell <cromwellnick@gmail.com>
2020-09-10 10:06:55 -0400

    Add Message Type and Version columns

--- src/Ranger.ApiGateway.Data/Migrations/20200910134342_AddOutboxMsgTypeVersion.Designer.cs
+++ src/Ranger.ApiGateway.Data/Migrations/20200910134342_AddOutboxMsgTypeVersion.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Ranger.ApiGateway.Data;
@@ -9,9 +10,10 @@ using Ranger.ApiGateway.Data;
 namespace Ranger.ApiGateway.Data.Migrations
 {
     [DbContext(typeof(ApiGatewayDbContext))]
-    partial class ApiGatewayDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200910134342_AddOutboxMsgTypeVersion")]
+    partial class AddOutboxMsgTypeVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
